1f8f8abeb1c0dcec0401c5244964a29285161d11
Jonathan Saúl Hidalgo <jonathansaul.it@gmail.com>
2023-02-12 22:08:21 -0300

    main

--- js/function.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("openVidZ").onclick = function(){
-    document.getElementById("modalContainerVidZ").style.opacity = 1
-}
-document.getElementById("closeVidZ").onclick = function(){
-    document.getElementById("modalContainerVidZ").style.opacity = 0
-}
